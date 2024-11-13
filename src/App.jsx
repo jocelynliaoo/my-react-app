b2da86e887a0f7866e2ed87922e9d197a20be6d0
Jocelyn Liao <jocelyn.l@wustl.edu>
2024-11-13 15:37:56 -0600

    add handleplay function

--- src/App.jsx
+++ src/App.jsx
@@ -76,21 +76,18 @@ function calculateWinner(squares) {
   return null;
 }
 
-function Board(){
-
-}
-
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <ol>{/*TODO*/}</ol>
-      </div>
-    </div>
-  );
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+  )
 }
