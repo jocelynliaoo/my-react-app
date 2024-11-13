dd6a333a7f928bb4442b05421508b243a37788a8
Jocelyn Liao <jocelyn.l@wustl.edu>
2024-11-13 15:40:15 -0600

    added onplay and history state to store information

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ export default function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = "O";
     }
+    onPlay(nextSquares);
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
@@ -82,7 +83,8 @@ export default function Game() {
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
-    // TODO
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
 
   return (
