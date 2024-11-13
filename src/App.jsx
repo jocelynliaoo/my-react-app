390787defa3b768263af298eaae69f990e19b03a
Jocelyn Liao <jocelyn.l@wustl.edu>
2024-11-13 15:32:04 -0600

    added game and board component

--- src/App.jsx
+++ src/App.jsx
@@ -75,3 +75,20 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+function Board(){
+
+}
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
